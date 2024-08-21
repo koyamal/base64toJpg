34fa3c12a3395a582efac15da3498298b8e9bb44
koyamal <tanakamaruko1121n@gmail.com>
2024-08-21 23:25:56 +0900

    修正中

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rs = fs.createReadStream('./base64.txt');
+const rl = readline.createInterface({
+  //読み込みたいストリームの設定
+    input: rs,
+  });
+
+  const line_counter = ((i = 0) => () => ++i)();
+  rl.on('line', (lineString, lineno = line_counter()) => {
+    //wsに一行ずつ書き込む
+    console.log(lineString[0]);
+    const base64Str = lineString.replace("data:image/jpeg;base64,","");
+    console.log(lineno)
+  });
+// fs.readFile("base64.txt", )
+// const inputBase64 = argv[1];
+// const base64Str = inputBase64.replace("data:image/jpeg;base64,","");
+
+// fs.promises.writeFile("test.jpeg", base64Str, { encoding: "base64" });
