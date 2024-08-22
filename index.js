a493cd50224685bad7cde57d5e1b09ba4f9ae7c3
koyamal <tanakamaruko1121n@gmail.com>
2024-08-22 19:57:45 +0900

    修正

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ const rl = readline.createInterface({
   input: rs,
 });
 
-const line_counter = ((i = 0) => () => ++i)();
-rl.on('line', (lineString, lineno = line_counter()) => {
-  const base64Str = lineString.replace("data:image/jpeg;base64,","");
-  fs.promises.writeFile(`images/${headText}_${lineno}.jpg`, base64Str, { encoding: "base64" });
-});
+const bfunc = () => "hello"
+const afunc = (() => bfunc)()
+
+
+console.log(afunc());
+
+const line_counter = ((i = 0) => {
+  return () => {
+    return ++i;
+  }
+})();
+console.log(line_counter())
+console.log(line_counter())
+// rl.on('line', (lineString, lineno = line_counter()) => {
+//   const base64Str = lineString.replace("data:image/jpeg;base64,","");
+//   fs.promises.writeFile(`images/${headText}_${lineno}.jpg`, base64Str, { encoding: "base64" });
+// });
