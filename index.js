f96a9843b0f1ca2e21d96ca2b6fdb812fe0ea067
koyamal <tanakamaruko1121n@gmail.com>
2024-08-23 08:03:24 +0900

    修正

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const readline = require('readline');
 
 const headText = process.argv[2];
 
-fs.mkdir(`images/${headText}`, (err) => {
-  if(err) {
-    console.log(err);
-    return;
-  }
-});
+const createJpg = async () => {
+  await fs.mkdir(`images/${headText}`, (err) => {
+    if(err) {
+      console.log(err);
+      return;
+    }
+  });
 
-const rs = fs.createReadStream(`images/${headText}.txt`);
-const rl = readline.createInterface({
-  input: rs,
-});
+  const rs = fs.createReadStream(`images/${headText}.txt`);
+  const rl = readline.createInterface({
+    input: rs,
+  });
+}
 
 const line_counter = ((i = 0) => {
   return () => {
@@ -25,3 +27,5 @@ rl.on('line', (lineString, lineno = line_counter()) => {
   const base64Str = lineString.replace("data:image/jpeg;base64,","");
   fs.promises.writeFile(`images/${headText}/${headText}_${lineno}.jpg`, base64Str, { encoding: "base64" });
 });
+
+console.log("終了");
