56770235c4869df307e8ef6c0ac36358fe5f635f
koyamal <tanakamaruko1121n@gmail.com>
2024-08-23 08:19:27 +0900

    修正

---
+++ main.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const headText = process.argv[2];
+
+const createJpg = async () => {
+  fs.mkdir(`images/${headText}`, (err) => {
+    if(err) {
+      console.log(err);
+      return;
+    }
+
+    const rs = fs.createReadStream(`images/${headText}.txt`);
+
+    const rl = readline.createInterface({
+      input: rs,
+    });
+
+    const line_counter = ((i = 0) => {
+      return () => {
+        return ++i;
+      }
+    })();
+
+    rl.on('line', (lineString, lineno = line_counter()) => {
+      const base64Str = lineString.replace("data:image/jpeg;base64,","");
+      fs.promises.writeFile(`images/${headText}/${headText}_${lineno}.jpg`, base64Str, { encoding: "base64" });
+    });
+
+    console.log("終了");
+  });
+};
+
+createJpg();
