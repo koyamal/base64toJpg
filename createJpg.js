2c72a11acb93d0f83dda1f6551fa57fd1a309cad
koyamal <74910401+koyamal@users.noreply.github.com>
2024-09-19 22:12:15 +0900

    修正

---
+++ createJpg.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const headText = process.argv[2];
+
+const createJpg = async () => {
+  fs.mkdir(`images/${headText}`, (err) => {
+    if(err) {
+      console.log(err);
+      return;
+    }
+
+    const rs = fs.createReadStream(`images/${headText}.txt`);
+
+    const rl = readline.createInterface({
+      input: rs,
+    });
+
+    const line_counter = ((i = 0) => {
+      return () => {
+        return ++i;
+      }
+    })();
+
+    rl.on('line', (lineString, lineno = line_counter()) => {
+      const base64Str = lineString.replace("data:image/jpeg;base64,","");
+      fs.promises.writeFile(`images/${headText}/${headText}_${lineno}.jpg`, base64Str, { encoding: "base64" });
+    });
+
+    console.log("完了しました。");
+  });
+};
+
+if(!headText) {
+  console.log("エラー: README.mdをご確認し、再度実行してください。");
+  return;
+}
+createJpg();
